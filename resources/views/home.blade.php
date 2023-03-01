296ce4e5295d4d03f3975e721aeff65fd81513c5
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-01 15:42:26 +0100

    finished application

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.main')
+
+@section('title', 'home')
+
+@section('content')
+    <table>
+        <thead>
+            <tr class="text-center">
+                <th>Train code</th>
+                <th>Company</th>
+                <th>Departure station</th>
+                <th>Arrival station</th>
+                <th>Departure time</th>
+                <th>Arrival time</th>
+                <th>Number of carriages</th>
+                <th>On time</th>
+                <th>Is cancelled</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr class="@if ($train->is_cancelled) cancelled @elseif (!$train->on_time) delayed @endif text-center">
+                    <td>{{ $train->train_code }}</td>
+                    <td>{{ $train->company }}</td>
+                    <td>{{ $train->departure_station }}</td>
+                    <td>{{ $train->arrival_station }}</td>
+                    <td>{{ $train->departure_time }}</td>
+                    <td>{{ $train->arrival_time }}</td>
+                    <td>{{ $train->number_of_carriages }}</td>
+                    <td>
+                        @if ($train->on_time)
+                            Yes
+                        @else
+                            No
+                        @endif
+                    </td>
+                    <td>
+                        @if ($train->is_cancelled)
+                            Yes
+                        @else
+                            No
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+@endsection
